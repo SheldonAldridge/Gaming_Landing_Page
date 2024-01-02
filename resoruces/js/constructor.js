e5b7b7e20e381a4629d3faef80c02b44131dc84c
Sheldon <sheldonaldridge@yahoo.com>
2024-01-02 22:05:31 +0200

    Create constructor.js

---
+++ resoruces/js/constructor.js
@@ -0,0 +1,8 @@
+export class item{
+    constructor(title,price,discount,discountPrice){
+        this.title = title;
+        this.price = price;
+        this.discount = discount;
+        this.discountPrice = discountPrice;
+    }
+}
